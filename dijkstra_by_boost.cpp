ec14ad6f28a65f29c85aa0cf3ecf7ac70924b724
Dmitriy_Like_Lemonade <dim4ik-super-klass@mail.ru>
2023-10-02 00:38:32 +0800

    example_complite

--- dijkstra_by_boost.cpp
+++ dijkstra_by_boost.cpp
@@ -1,33 +1,73 @@
+#include <boost/config.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
+#include <climits>
 int main(int args, char *argv[]){
-    using EdgeWeightProperty = boost::property<boost::edge_weight_t, int>;
-    using DirectedGraph = boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, EdgeWeightProperty>;
-    using edge_iterator = boost::graph_traits<DirectedGraph>::edge_iterator;
-    using EdgeMapProperty = boost::property_map<DirectedGraph, boost::edge_weight_t>::type;
-
-    DirectedGraph g;
-
-    boost::add_edge (0, 1, 3, g);
-    boost::add_edge (0, 2, 1, g);
-    boost::add_edge (0, 3, 7, g);
-    boost::add_edge (1, 3, 6, g);
-    boost::add_edge (2, 3, 9, g);
+    using edge_weight_property = boost::property<boost::edge_weight_t, size_t>;
+    using directed_graph = boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, edge_weight_property>;
+    using edge_iterator = boost::graph_traits<directed_graph>::edge_iterator;
+    using weight_of_edges_map_property = boost::property_map<directed_graph, boost::edge_weight_t>::type;
+    using vertex_descriptor = boost::graph_traits<directed_graph>::vertex_descriptor;
 
 
-    EdgeMapProperty edgeWeightMap = get(boost::edge_weight_t(), g);
+    directed_graph graph;
 
-    std::pair<edge_iterator, edge_iterator> ei = edges(g);
-
-    std::cout << "Количество ребер = " << num_edges(g) << "\n";
-    std::cout << "Список ребер:\n";
-
-    std::for_each(ei.first, ei.second, [&](auto edge){
-        std::cout << edge << " " << edgeWeightMap[edge] << std:: endl;
-    });
-    std::cout << std::endl;
+    int f, g;
+    std::ifstream fin(argv[1]);
+    fin >> f;
+    fin >> f;
+    fin >> g;
     
+    --f;
+    --g;
+    
+    std::string line;
+    std::stringstream buf;
+
+    std::getline(fin, line);
+    
+    int from, to, weight;
+    
+    while(std::getline(fin, line)){
+        buf << line;
+        
+        buf >> from >> to >> weight;
+        --from;
+        --to;
+        
+        boost::add_edge(from, to, weight, graph);
+
+        buf.clear();
+    }   
+
+
+
+    vertex_descriptor start = vertex(f, graph);
+    vertex_descriptor end = vertex(g, graph);
+
+    std::vector<vertex_descriptor> predecessors(num_vertices(graph));
+    std::vector<int> distances(num_vertices(graph));
+    
+    boost::dijkstra_shortest_paths(graph, start, boost::predecessor_map(&predecessors[0]).distance_map(&distances[0]));
+
+
+
+    std::vector<vertex_descriptor> path; 
+    auto current = end;
+    do{
+        path.push_back(current);
+        current = predecessors[current];
+    }while(current != start);
+
+
+    std::cout << "Path from " << start + 1 << " to " << end + 1 << " = " << distances[end] << '\n';
+    std::copy(path.begin(), path.end(), std::ostream_iterator<vertex_descriptor>(std::cout, " -> "));
+    std::cout << ++start << std::endl;
     
     
     return 0;
