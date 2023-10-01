551e664e40dfccb72c32958c0fa8119f0db17710
Dmitriy <32592907+Lukashevskiy@users.noreply.github.com>
2023-10-01 20:52:25 +0800

    Delete build directory
    my fault. DONT FORGET TO CHECK GITIGNORE

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitriy/algos/dijkstra
-# Build directory: /home/dmitriy/algos/dijkstra/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
